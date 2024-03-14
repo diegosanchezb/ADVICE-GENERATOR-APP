95a33aa8e231b419d034515b6570b629a7977c5d
Diego Sánchez <sanchezbrisadiego@gmail.com>
2024-03-14 09:42:56 -0300

    Primer commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+const diceBtn = document.querySelector('#diceBtn');
+const advice = document.querySelector('#advice');
+const adviceId = document.querySelector('#adviceId');
+
+getAdvice()
+
+diceBtn.addEventListener('click',()=>{
+    
+    getAdvice()
+
+})
+
+function getAdvice() {
+    const id = Math.floor(Math.random()*100)+1;
+
+    fetch(`https://api.adviceslip.com/advice/${id}`)
+     .then(res=>res.json())
+     .then(data => {
+        const adviceMsg = data.slip.advice;
+        advice.innerText = adviceMsg;
+        adviceId.innerText = data.slip.id;
+    })
+    .catch(err => getAdvice())
+}
+
+// 1:43
